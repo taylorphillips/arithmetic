9edb8ef729dafd67786c6821a4f3d7ae53ecca46
tcp <phillitc@gmail.com>
2022-08-04 21:23:13 -0700

    Block refactor; snapping works!

--- scripts/Block.cs
+++ scripts/Block.cs
@@ -4,17 +4,12 @@ using Object = Godot.Object;
 
 public class Block : Node2D
 {
-    public ConnectorArea2D inputConnector;
-    public ConnectorArea2D outputConnector;
-    public Vector2? snapPosition;
-
     public class SelectableArea2D : Area2D
     {
         private Polygon2D poly;
-        private bool selected = false;
 
         public override void _Ready() {
-            // Create body
+            Name = "SelectableArea2D";
             poly = new Polygon2D();
             poly.Color = Colors.Red;
             poly.Polygon = new Vector2[] {
@@ -34,34 +29,9 @@ public class Block : Node2D
         public override void _InputEvent(Object viewport, InputEvent @event, int shapeIdx) {
             if (@event is InputEventMouseButton mouseEvent) {
                 if (mouseEvent.Pressed && mouseEvent.ButtonIndex == (int) ButtonList.Left) {
-                    GD.Print("LEFT CLICK");
-                    selected = true;
+                    GetParent<Block>().select();
                 } else if (!mouseEvent.Pressed && mouseEvent.ButtonIndex == (int) ButtonList.Left) {
-                    GD.Print("LEFT UNCLICK");
-                    selected = false;
-                }
-            }
-        }
-
-        public void _OnMouseEnter() {
-            poly.Color = Colors.Blue;
-        }
-
-        public void _OnMouseExit() {
-            poly.Color = Colors.Red;
-        }
-
-        public override void _PhysicsProcess(float delta) {
-            if (selected) {
-                Block block = GetParent<Block>();
-                if (block.snapPosition.HasValue) {
-                    Transform2D transform = GlobalTransform;
-                    transform.origin = block.snapPosition.Value;
-                    GetParent<Node2D>().Transform = transform;
-                } else {
-                    Transform2D transform = GlobalTransform;
-                    transform.origin = GetGlobalMousePosition();
-                    GetParent<Node2D>().Transform = transform;
+                    GetParent<Block>().deselect();
                 }
             }
         }
@@ -75,10 +45,8 @@ public class Block : Node2D
             OUTPUT = 2,
         }
 
-        private readonly ConnectorType connectorType;
-        private readonly Vector2 initPosition;
-        private ConnectorArea2D connectedConnector;
-        private ConnectorArea2D snappedConnector;
+        public readonly Vector2 initPosition;
+        public readonly ConnectorType connectorType;
 
         public ConnectorArea2D(ConnectorType connectorType, Vector2 initPosition) {
             this.connectorType = connectorType;
@@ -86,15 +54,14 @@ public class Block : Node2D
         }
 
         public override void _Ready() {
+            Name = "ConnectorArea2D-" + connectorType;
             CircleShape2D circle = new CircleShape2D();
             circle.Radius = 20;
             CollisionShape2D collisionShape2D = new CollisionShape2D();
             collisionShape2D.Shape = circle;
             Position = initPosition;
             AddChild(collisionShape2D);
-            Name = "Connector" + connectorType;
             Connect("area_entered", this, "_OnAreaEnter");
-            Connect("area_exited", this, "_OnAreaExit");
         }
 
         public bool ConnectsTo(ConnectorArea2D connector) {
@@ -102,35 +69,83 @@ public class Block : Node2D
         }
 
         public void _OnAreaEnter(Area2D area2D) {
-            GD.Print(Name + " has been entered by " + area2D.Name);
             if (area2D is ConnectorArea2D connectorArea2D && ConnectsTo(connectorArea2D)) {
-                snappedConnector = connectorArea2D;
-                GetParent<Block>().snapPosition = snapToPosition(snappedConnector);
+                GetParent<Block>().connectorCollision(this, connectorArea2D);
             }
-        }
-
-        // TODO: This never triggers, figure out how to fix.
-        public void _OnAreaExit(Area2D area2D) {
-            GD.Print(Name + " has been exited by " + area2D.Name);
-            if (snappedConnector != null && area2D is ConnectorArea2D connectorArea2D) {
-                if (connectorArea2D.GetPath() == snappedConnector.GetPath()) {
-                    snappedConnector = null;
-                    GetParent<Block>().snapPosition = null;
-                }
-            }
-        }
-        
-        private Vector2 snapToPosition(ConnectorArea2D connectorArea2D) {
-            // TODO: Make this correct.
-            return connectorArea2D.GlobalPosition;
         }
     }
 
+    private ConnectorArea2D inputConnector;
+    private ConnectorArea2D outputConnector;
+
+    private ConnectorArea2D connectedConnector;
+    private ConnectorArea2D snappedConnector;
+
+    private ConnectorArea2D snapFrom;
+    private ConnectorArea2D snapTo;
+    private bool selected = false;
+
     public override void _Ready() {
         AddChild(new SelectableArea2D());
-        inputConnector = new ConnectorArea2D(ConnectorArea2D.ConnectorType.INPUT, new Vector2(0, -40));
+        inputConnector = new ConnectorArea2D(ConnectorArea2D.ConnectorType.INPUT, new Vector2(0, 40));
         AddChild(inputConnector);
-        outputConnector = new ConnectorArea2D(ConnectorArea2D.ConnectorType.OUTPUT, new Vector2(0, 40));
+        outputConnector = new ConnectorArea2D(ConnectorArea2D.ConnectorType.OUTPUT, new Vector2(0, -40));
         AddChild(outputConnector);
+    }
+
+    public override void _PhysicsProcess(float delta) {
+        if (selected) {
+            Transform2D transform = GlobalTransform;
+            transform.origin = GetGlobalMousePosition();
+            if (getSnapPosition().HasValue) {
+                if (GlobalPosition.DistanceTo(GetGlobalMousePosition()) > 40f) {
+                    snapFrom = null;
+                    snapTo = null;
+                } else {
+                    transform.origin = getSnapPosition().Value;
+                }
+            }
+
+            Transform = transform;
+        }
+    }
+
+    private Vector2 getSnapPosition(ConnectorArea2D from, ConnectorArea2D to) {
+        if (from.connectorType == ConnectorArea2D.ConnectorType.INPUT) {
+            return to.GlobalPosition + new Vector2(0, -40);
+        } else {
+            return to.GlobalPosition + new Vector2(0, 40);
+        }
+    }
+
+    private Vector2? getSnapPosition() {
+        if (snapFrom != null && snapTo != null) {
+            return getSnapPosition(snapFrom, snapTo);
+        } else {
+            return null;
+        }
+    }
+
+    private void connectorCollision(ConnectorArea2D snapFrom, ConnectorArea2D snapTo) {
+        if (selected) {
+            this.snapFrom = snapFrom;
+            this.snapTo = snapTo;
+        }
+    }
+
+    private void select() {
+        selected = true;
+    }
+
+    private void deselect() {
+        selected = false;
+
+        // Confirm the snap
+        if (snapFrom != null && snapFrom != null) {
+            // TODO: Add to parent 2D as group
+            // TODO: Store the graph somewhere.
+            snapFrom = null;
+            snapTo = null;
+        }
     }
 }
